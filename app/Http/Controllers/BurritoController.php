afe60e8e3fc8396ebc7195e14cc953b526111269
Connor Smyth <connor@artscience.ca>
2021-01-16 17:54:00 -0500

    Fixing dumb bug where I forgot to return the response in the main function, d'oh

--- app/Http/Controllers/BurritoController.php
+++ app/Http/Controllers/BurritoController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class BurritoController extends Controller {
 	public function giveBurrito( Request $request ) {
-		$this->slackChallenge( $request );
+		return $this->slackChallenge( $request );
 	}
 	
 	private function slackChallenge( Request $request ) {
@@ -18,7 +17,8 @@ class BurritoController extends Controller {
 			return response()
 				->json( [
 					'challenge' => $challenge
-				] );
+				] )
+				->header( 'Access-Control-Allow-Origin', '*' );
 		}
 		return response( 'No payload in request' )
 			->header( 'Content-Type', 'text/plain' );
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\BurritoController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware( 'auth:api' )->get( '/user', function( Request $request ) {
+	return $request->user();
+} );
 
 //Route::middleware('auth:api')->post('/burrito', [BurritoController::class, 'giveBurrito']);
 Route::post('/burrito', [BurritoController::class, 'giveBurrito']);
