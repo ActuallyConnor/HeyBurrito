91f954e6f105f5d13c555df745a1bd838d11f700
Connor Smyth <connor@artscience.ca>
2021-01-16 16:54:34 -0500

    Setting up slack challenge response

--- app/Http/Controllers/BurritoController.php
+++ app/Http/Controllers/BurritoController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BurritoController extends Controller {
-	public function giveBurrito() {
+	public function giveBurrito(Request $request) {
+		$this->slackChallenge($request);
+	}
 	
+	/**
+	 * @param Request $request
+	 * @return false|Response
+	 */
+	private function slackChallenge(Request $request): Response|bool {
+		if ($request->has('challenge')) {
+			$requestJson = $request->input('challenge');
+			$requestArr = json_decode($requestJson, true);
+			
+			return response($requestArr['challenge'])
+				->header('Content-type', 'text/plain' );
+		}
+		return false;
 	}
 }
