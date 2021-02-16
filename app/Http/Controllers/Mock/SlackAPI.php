60305080512cb72e1dc17319bbbbdf8482075f4b
Connor Smyth <connor@artscience.ca>
2021-02-16 11:20:55 -0500

    Mocking slack api for easier testing so that we don't hit rate limits on tests

---
+++ app/Http/Controllers/Mock/SlackAPI.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Mock;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SlackAPI extends Controller {
+
+    public function usersList( Request $request ) {
+
+    }
+
+}
