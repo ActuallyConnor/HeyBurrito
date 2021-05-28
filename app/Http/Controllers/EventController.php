324e86f254161e3ee86e3ad437cdc4269f03b558
Connor Smyth <connor@artscience.ca>
2021-05-28 17:26:06 -0400

    Committing last state

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\SlackEvent;
 use App\Http\Requests\EventPostRequest;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller {
@@ -21,7 +22,7 @@ class EventController extends Controller {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index() {
         return response( 'GET /api/event' );
@@ -30,7 +31,7 @@ class EventController extends Controller {
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create() {
         //
@@ -39,9 +40,8 @@ class EventController extends Controller {
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     * @throws \Illuminate\Validation\ValidationException
+     * @param EventPostRequest $request
+     * @return Response
      */
     public function store( EventPostRequest $request ) {
         $validated = $request->validated();
@@ -69,7 +69,7 @@ class EventController extends Controller {
      * Display the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show( $id ) {
         //
@@ -79,7 +79,7 @@ class EventController extends Controller {
      * Show the form for editing the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit( $id ) {
         //
@@ -88,9 +88,9 @@ class EventController extends Controller {
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update( Request $request, $id ) {
         //
@@ -100,7 +100,7 @@ class EventController extends Controller {
      * Remove the specified resource from storage.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy( $id ) {
         //
